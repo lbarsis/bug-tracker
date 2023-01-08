0fe099cfb15ce6e7d292102d2edca915e8c277df
Levi Barsis <lbarsis@gmail.com>
2023-01-07 21:47:57 -0800

    Made tickets table collapsible

--- bug_tracker_client/src/components/ProjectRow.js
+++ bug_tracker_client/src/components/ProjectRow.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TicketTable from './TicketTable';
 
 function ProjectRow({ project }) {
   const { id, name, description, status, created_at, tickets } = project
+  const [showTickets, setShowTickets] = useState(false)
 
   const displayTickets = tickets.map(ticket => {
     return <TicketTable key={ticket.id} ticket={ticket} />
   })
+
+  // changes state from true to false
+  function onDisplayTickets() {
+    setShowTickets(displayTickets => !displayTickets)
+  }
 
   return (
     <>
@@ -14,26 +20,32 @@ function ProjectRow({ project }) {
         <td>{name}</td>
         <td>{description}</td>
         <td>{status}</td>
-        <td>{tickets.length}</td>
+        <td onClick={onDisplayTickets}>{tickets.length}</td>
         <td>{created_at}</td>
       </tr>
+      {/* ---- tickets section ---- */}
       <tr>
-        <td colSpan="5">
-          <table className='ticket-table'>
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Priority</th>
-              <th>Description</th>
-              <th>Status</th>
-              <th>Hours</th>
-              <th>User</th>
-              <th>Created At</th>
-            </tr>
-          </thead>
-          {displayTickets}
-          </table>
-        </td>
+        {/* determines if ticket table should be displayed based on state */}
+        {showTickets ?
+          <td colSpan="5">
+            <table className='ticket-table'>
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Priority</th>
+                  <th>Description</th>
+                  <th>Status</th>
+                  <th>Hours</th>
+                  <th>User</th>
+                  <th>Created At</th>
+                </tr>
+              </thead>
+              {displayTickets}
+            </table>
+          </td>
+          :
+          null
+        }
       </tr>
     </>
   )
